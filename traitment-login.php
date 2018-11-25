d884a67e3051b76a4263cfc4a225272ecba5a772
Buck <elie@gazel.net>
2018-11-25 16:24:34 +0100

    Ppe gestion fal

--- traitment-login.php
+++ traitment-login.php
@@ -12,21 +12,21 @@ $login= htmlspecialchars($_POST['login']);
 $pass= htmlspecialchars($_POST['password']);
 $recovery_user = $connect->prepare("SELECT * FROM USER WHERE SURNOM_USER = ? AND PASSWORD = ?");
 $recovery_user->execute(array($login,sha1($pass)));
-if($recovery_user->rowcount() == 1){
+if($recovery_user->rowcount() == 1) {
     $info_user = $recovery_user->fetch();
-    $_SESSION['ID_USER']=$info_user['ID_USER'];
-    $_SESSION['NOM_USER']=$info_user['NOM_USER'];
-    $_SESSION['PRENOM_USER']=$info_user['PRENOM_USER'];
-    $_SESSION['LOGIN']=$info_user['SURNOM_USER'];
-    $_SESSION['PASSWORD']=$info_user['PASSWORD'];
-    $_SESSION['ROLE_USER']=$info_user['ROLE_USER'];
+    $_SESSION['ID_USER'] = $info_user['ID_USER'];
+    $_SESSION['NOM_USER'] = $info_user['NOM_USER'];
+    $_SESSION['PRENOM_USER'] = $info_user['PRENOM_USER'];
+    $_SESSION['LOGIN'] = $info_user['SURNOM_USER'];
+    $_SESSION['PASSWORD'] = $info_user['PASSWORD'];
+    $_SESSION['ROLE_USER'] = $info_user['ROLE_USER'];
 
-    if($_SESSION['ROLE_USER']=="impetrant"){
-        header('location:./index_impetrant.html');
-    } elseif ($_SESSION['ROLE_USER']=="fal"){
-        header('location:./index_fal.html');
-    } elseif ($_SESSION['ROLE_USER']=="croix"){
-        header('location:./index_croix.html');
+    if ($_SESSION['ROLE_USER'] == "impetrant") {
+        header('location: index_impetrant.html');
+    } elseif ($_SESSION['ROLE_USER'] == "fal") {
+        header('location: index_fal.html');
+    } elseif ($_SESSION['ROLE_USER'] == "croix") {
+        header('location: index_croix.html');
     }
 }
 ?>
--- traitment-register.php
+++ traitment-register.php
@@ -1,27 +1,29 @@
 <?php
 /* if (isset($_POST['submit'])){
 } */
-$nom=htmlspecialchars($_POST['name']);
-$prenom=htmlspecialchars($_POST['surname']);
-$nick=htmlspecialchars($_POST['nickname']);
-$pass=htmlspecialchars($_POST['password']);
-$pass=sha1($pass);
-$date=htmlspecialchars($_POST['datebapt']);
-$mail=htmlspecialchars($_POST['mail']);
-$filiere=htmlspecialchars($_POST['filiere']);
-$mailAuth=htmlspecialchars($_POST['mailAuth']);
-$tel=htmlspecialchars($_POST['tel']);
-$role=htmlspecialchars($_POST['radioRank']);
-try
-{
-    $connect = new PDO('mysql:host=localhost;dbname=GestionFal;charset=utf8', 'root', 'password');
-}
-catch (Exception $e)
-{
-    die('Erreur : ' . $e->getMessage());
-}
+if($_POST) {
+    $nom = htmlspecialchars($_POST['name']);
+    $prenom = htmlspecialchars($_POST['surname']);
+    $nick = htmlspecialchars($_POST['nickname']);
+    $pass = htmlspecialchars($_POST['password']);
+    $pass = sha1($pass);
+    $date = htmlspecialchars($_POST['datebapt']);
+    $mail = htmlspecialchars($_POST['mail']);
+    $filiere = htmlspecialchars($_POST['filiere']);
+    $mailAuth = htmlspecialchars($_POST['mailAuth']) == "on";
+    $tel = htmlspecialchars($_POST['tel']);
+    $role = htmlspecialchars($_POST['optradio']);
+    try {
+        $connect = new PDO('mysql:host=localhost;dbname=GestionFal;charset=utf8', 'root', 'password');
+        $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+    }
 
-$insert = $connect->prepare("INSERT INTO USER (`NOM_USER`,`PRENOM_USER`,`SURNOM_USER`,`PASSWORD`,`ROLE_USER`,`DATEBAPT_USER`,`FILIERE_USER`,`TEL_USER`,`MAIL_USER`, `MAILAUTH_USER`) VALUES(?,?,?,?,?,?,?,?,?,?)");
-$insert->execute(array($nom,$prenom,$nick,$pass,$role,$date,$filiere,$tel,$mail,$mailAuth)) or die(print_r($insert->errorInfo(), true));
-header('location:index.html')
+    $insert = $connect->prepare("INSERT INTO user (ID_USER, NOM_USER, PRENOM_USER, SURNOM_USER, PASSWORD, DATEBAPT_USER, TEL_USER, MAIL_USER, MAILAUTH_USER) VALUES (?,?,?,?,?,?,?,?,?)");
+    $insert->execute(array(null, $nom, $prenom, $nick, $pass, $date, $tel, $mail, $mailAuth));
+
+    /*echo $connect->lastInsertId();*/
+    /*header('location:index.html')*/
+}
 ?>
